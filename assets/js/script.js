cd26b3889ac68a41bafe9db97d285ba3ff255e8a
undang-digital-aja <undangdigitalaja@gmail.com>
2022-06-24 12:41:31 +0700

    add dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-// ===== LOADER ===== //
-onload = () =>{
-	const load = document.getElementById('load');
-
-	setTimeout(() =>{
-		load.style.display = 'none'
-	}, 2800)
-}
-// ===== end loader ===== //
-
 // ===== SHOW MENU ===== //
 const navMenu = document.getElementById('nav-menu'),
 		navToggle = document.getElementById('nav-toggle'),
@@ -67,7 +57,7 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader);
 // // ===== end change background header ===== //
 
-// // ===== PRODUCTS CAROUSEL ===== //
+// ===== PRODUCTS CAROUSEL ===== //
 const blogSwiper = new Swiper('.products__container', {
 	spaceBetween: 24,
 	loop: true,
@@ -93,9 +83,9 @@ const blogSwiper = new Swiper('.products__container', {
 		},
 	}
 });
-// // ===== end blog carousel ===== //
+// ===== end blog carousel ===== //
 
-// // ===== GALLERY ===== //
+// ===== GALLERY ===== //
 const gallerySwiper = new Swiper(".gallery__container", {
 	spaceBetween: 0,
 	loop: 'true',
@@ -118,7 +108,7 @@ const gallerySwiper = new Swiper(".gallery__container", {
 		},
 	},
 })
-// // ===== end gallery ===== //
+// ===== end gallery ===== //
 
 // ===== SCROLL UP ===== //
 function scrollUp(){
@@ -129,10 +119,35 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp);
 // ===== end scroll up ===== //
 
+// ===== DARK LIGHT THEME ===== //
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
+// ===== end dark light theme ===== //
+
 // ===== AOS ANIMATE ===== //
 // 1. home -> .home__content
 
-// // 2. fitur -> .fitur__box
+// 2. fitur -> .fitur__box
 const fiturCard = document.querySelectorAll('.fitur__card');
 fiturCard.forEach((n, i) => {
 	n.dataset.aos = 'fade-down';
